89234afc853e83e82352942eb0fdc6965d285d9b
Tracey Lee <traceylg43@gmail.com>
2025-09-12 20:59:45 -0400

    add id to error spans and validation js

---
+++ scripts/validation.js
@@ -0,0 +1,31 @@
+const showInputError = (formEl, inputEl, errorMsg) => {};
+
+const checkInputValidity = (formEl, inputEl) => {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  }
+};
+
+const setEventListeners = (formEl) => {
+  const inputList = formEl.querySelectorAll(".modal__input");
+  const buttonEl = formEl.querySelectorAll("modal__button");
+
+  //todd - handle intial states
+  //toggleButtonState(inputList, buttonEl);
+
+  inputList.forEach((inputEl) => {
+    inputEl.addEventListener("input", function () {
+      checkInputValidity(formEl, inputEl);
+      //toggleButtonState(inputList, buttonEl);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
